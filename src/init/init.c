49bd638f281c9c19fbaed5d3fb495bea7b7f907b
Abusalah0 <109g83@gmail.com>
2025-06-06 01:15:31 +0300

    norminette init/

--- src/init/init.c
+++ src/init/init.c
@@ -6,34 +6,38 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 23:55:29 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/05/31 19:14:26 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/06/06 01:14:58 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-static void    init_map(t_game *game)
+static void	init_map(t_game *game)
 {
-    ft_bzero(&game->map, sizeof(t_map));
-    ft_bzero(&game->player, sizeof(t_player));
+	ft_bzero(&game->map, sizeof(t_map));
+	ft_bzero(&game->player, sizeof(t_player));
 }
 
-static void    check_args(int ac, char **av)
+static void	check_args(int ac, char **av)
 {
-    if (ac != 2)
-    {
-        ft_exit_handler(NULL, (char *[]){"Error\nInvalid number of arguments.\nUsage: ./cube3d map.cub", NULL}, 1, NULL);
-    }
-    if (ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4) != 0)
-    {
-        ft_exit_handler(NULL, (char *[]){"Error\nInvalid file extension. Expected .cub", NULL}, 1, NULL);
-    }
+	if (ac != 2)
+	{
+		ft_exit_handler(NULL,
+			(char *[]){"Error\nInvalid number of arguments.\nUsage: \
+./cube3d map.cub\n", NULL}, 1, NULL);
+	}
+	if (ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4) != 0)
+	{
+		ft_exit_handler(NULL,
+			(char *[]){"Error\nInvalid file extension. Expected .cub", NULL}, 1,
+			NULL);
+	}
 }
 
-void    init(t_game *game, int ac, char **av)
+void	init(t_game *game, int ac, char **av)
 {
-    check_args(ac, av);
-    init_mlx(game);
-    init_map(game);
-    game->fd = -1;
+	check_args(ac, av);
+	init_mlx(game);
+	init_map(game);
+	game->fd = -1;
 }
--- src/init/init_mlx.c
+++ src/init/init_mlx.c
@@ -6,23 +6,25 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 05:59:31 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/05/06 06:35:30 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/06/06 01:13:12 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cube3d.h" 
+#include "cube3d.h"
 
-void    init_mlx(t_game *game)
+void	init_mlx(t_game *game)
 {
-    game->mlx = mlx_init(W_WIDTH, W_HEIGHT, "Cube3D", true);
-    if (!game->mlx)
-    {
-        ft_exit_handler(game, (char *[]){"Error\nFailed to initialize mlx.\n", NULL}, 1, NULL);        
-    }
-    game->img = mlx_new_image(game->mlx, W_WIDTH, W_HEIGHT);
-    if (!game->img)
-    {
-        mlx_close_window(game->mlx);
-        ft_exit_handler(game, (char *[]){"Error\nFailed to create image.\n", NULL}, 1, NULL);        
-    }
+	game->mlx = mlx_init(W_WIDTH, W_HEIGHT, "Cube3D", true);
+	if (!game->mlx)
+	{
+		ft_exit_handler(game, (char *[]){"Error\nFailed to initialize mlx.\n",
+			NULL}, 1, NULL);
+	}
+	game->img = mlx_new_image(game->mlx, W_WIDTH, W_HEIGHT);
+	if (!game->img)
+	{
+		mlx_close_window(game->mlx);
+		ft_exit_handler(game, (char *[]){"Error\nFailed to create image.\n",
+			NULL}, 1, NULL);
+	}
 }
