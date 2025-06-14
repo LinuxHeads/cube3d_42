2eac5e96e353a446898234a56881ed7682243c85
Abusalah0 <109g83@gmail.com>
2025-06-14 03:26:17 +0300

    split the exit handler into files and functions

--- src/exit_handler/exit_handler.c
+++ src/exit_handler/exit_handler.c
@@ -6,52 +6,11 @@
 /*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 02:50:13 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/06/06 05:13:05 by abdsalah         ###   ########.fr       */
+/*   Updated: 2025/06/14 03:25:14 by abdsalah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
-
-void	free_mlx(t_game *game)
-{
-	if (game->img)
-		mlx_delete_image(game->mlx, game->img);
-	if (game->mlx)
-	{
-		mlx_terminate(game->mlx);
-	}
-	game->img = NULL;
-	game->mlx = NULL;
-}
-
-void	free_map(t_game *game)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < game->map.height)
-	{
-		free(game->map.content[i]);
-		i++;
-	}
-	free(game->map.content);
-	game->map.content = NULL;
-	if (game->map.buffer)
-		ft_lstclear(&game->map.buffer, free);
-	game->map.buffer = NULL;
-	i = -1;
-	while (++i < 4)
-	{
-		if (game->map.texture[i])
-		{
-			free(game->map.texture[i]);
-			game->map.texture[i] = NULL;
-		}
-	}
-	if (game->fd > 0)
-		close(game->fd);
-	game->fd = -1;
-}
 
 void	print_error_message(char **message)
 {
---
+++ src/exit_handler/free_map.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/14 03:24:13 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/06/14 03:24:42 by abdsalah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+static void	free_map_textures(t_game *game)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < 4)
+	{
+		if (game->map.texture[i])
+		{
+			free(game->map.texture[i]);
+			game->map.texture[i] = NULL;
+		}
+	}
+}
+
+static void	free_map_content(t_game *game)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < game->map.height)
+	{
+		printf("i = %ld\n", i);
+		if (game->map.content[i])
+			free(game->map.content[i]);
+		else
+			break ;
+		i++;
+	}
+}
+
+void	free_map(t_game *game)
+{
+	if (game->map.height && game->map.width)
+	{
+		free_map_content(game);
+	}
+	if (game->map.content)
+		free(game->map.content);
+	game->map.content = NULL;
+	if (game->map.buffer)
+		ft_lstclear(&game->map.buffer, free);
+	game->map.buffer = NULL;
+	free_map_textures(game);
+	if (game->fd > 0)
+		close(game->fd);
+	game->fd = -1;
+}
